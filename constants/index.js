bf6133b3d5001e0119ca3caf05f9bb6a3695c81c
MichaelJs <147663415+Michaecyber@users.noreply.github.com>
2024-02-28 22:25:18 +0200

    add: initial files

---
+++ constants/index.js
@@ -0,0 +1,89 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'Sublimation',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'Vinyl - Car Branding',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'PVC',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Flags and Pamphlets',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Pullup Banners',
+  },
+];
+
+export const startingFeatures = [
+  'Offset Printing: Harnessing traditional printing methods with cutting-edge technology, we deliver high-quality offset printing for a range of products including brochures, flyers, and posters.',
+  'Large Format Printing: Make a statement with our large format printing capabilities. From banners and signs to trade show displays, we will help you capture attention on a grandscale.',
+  'Digital Printing: For quick turnarounds and personalized projects, our digital printing services are perfect. Whether you need short runs of flyers or customized invitations, we got you covered.',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'Printing services',
+    subtitle:
+        'Elevate your prints with our range of finishing options including laminating, binding, and embossing.',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'Large format printing',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Drive Your Brand Forward with Custom Car Branding Solutions',
+    subtitle:
+        'Vehicle Wraps: Wrap your vehicle in stunning graphics and designs that showcase your brand identity with style.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'Dye-sublimation printing',
+    subtitle:
+        'Leave a lasting impression with personalized promotional items. From mugs and phone cases to mouse pads and keychains.',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'Large Format Printing',
+    subtitle:
+        'Make a statement with larger-than-life prints. Whether it is banners, flags, or trade show displays, our large format dye-sublimation printing will grab attention and turn heads.',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+    href: '/https://web.facebook.com/profile.php?id=100063515502173',
+  },
+];
---
+++ starter_repo/constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'Graphic designing',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'Sublimation',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Vinyl Printing',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'PVC',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'T-shirts',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  'Enter the world by reading basmalah to be safe',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'Quality Assurance',
+    subtitle:
+        'We adhere to strict quality control measures to ensure that every print meets the highest standards of excellence.',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'Competitive Pricing',
+    subtitle:
+        'Our dedicated team of experts is committed to finding creative solutions to bring your ideas to life, no matter where you are in the world',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Online Printing Services',
+    subtitle:
+        'Seamlessly order prints online and have them delivered anywhere in the world. Our user-friendly platform makes printing convenient and hassle-free.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'International Shipping',
+    subtitle:
+        'With our global shipping network, we ensure timely delivery of your prints to any corner of the globe, so you can reach your audience wherever they may be.',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'Digital Printing',
+    subtitle:
+        'From short runs to personalized prints, our digital printing capabilities ensure fast, flexible, and precise outcomes for your projects.',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
